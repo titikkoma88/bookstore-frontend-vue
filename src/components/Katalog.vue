107e242059949f8bd88881b11c49b0750a46c693
titikkoma88 <jie.koma88@gmail.com>
2021-08-16 10:32:21 +0700

    menampilkan halaman Katalog

--- src/components/Katalog.vue
+++ src/components/Katalog.vue
@@ -1,12 +1,36 @@
 <template>
     <div class="container mt-3 w-75">
         <h1>Selamat datang di toko buku Camp404 !</h1>
+        <div id="katalogBuku">
+            <h2 class="mt-5">Katalog Buku</h2>
+            <hr />
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Judul</th>
+                        <th>Pengarang</th>
+                        <th>Harga</th>
+                        <th>Stok</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(book, index) in bookList" :key="index">
+                        <td>{{index + 1}}</td>
+                        <td>{{book.judul}}</td>
+                        <td>{{book.pengarang}}</td>
+                        <td>{{book.harga}}</td>
+                        <td>{{book.stok}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: ["bookList"],
 }
 </script>
 
